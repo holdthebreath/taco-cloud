ff40b3b25caf8da8613871a0576bc3d0ac6c7d68
holdthebreath <hwdxxy@gmail.com>
2020-07-08 21:12:18 +0800

    章节2.3内容(design页面显示校验错误信息)

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Data
 public class Taco {
     @NotBlank
-    @Size(min = 3, message = "Name must be least 3 characters long")
+    @Size(min = 3, message = "Name must be at least 3 characters long")
     private String name;
     @NotEmpty
     @Size(min = 1, message = "You must choose at least 1 ingredient")
