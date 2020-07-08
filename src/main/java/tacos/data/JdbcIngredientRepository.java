bb7f1750e5a81d0335db0a53359722935c3e7a37
holdthebreath <hwdxxy@gmail.com>
2020-07-08 22:00:00 +0800

    章节3.1内容(使用JdbcTemplate)

--- src/main/java/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -15,11 +15,14 @@ import java.sql.SQLException;
  * @Date 2020/7/8 9:43 PM
  * @Version 1.0
  */
+//构造型注解
 @Repository
-public class JdbcIngredientRepository implements IngredientRepository{
+public class JdbcIngredientRepository implements IngredientRepository {
     private JdbcTemplate jdbc;
+
+    //    创建JdbcIngredientRepository bean时,通过@Autowired标注的构造器将JdbcTemplate注入
     @Autowired
-    public JdbcIngredientRepository(JdbcTemplate jdbc){
+    public JdbcIngredientRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
     }
 
@@ -31,12 +34,21 @@ public class JdbcIngredientRepository implements IngredientRepository{
     }
 
     @Override
-    public Iterable<Ingredient> findAll(){
+    public Iterable<Ingredient> findAll() {
+//        query()接收执行的sql以及Spring RowMapper的一个实现(用以将结果集中的每行数据映射为一个对象)
+//        query()还能以最终参数形式接收查询所需的任意参数
         return jdbc.query("select id, name, type from Ingredient", this::mapRowToIngredient);
     }
 
     @Override
-    public Ingredient findOne(String id){
-        return jdbc.queryForObject("select id, name, type from Ingredient where id = ?", this::mapRowToIngredient);
+    public Ingredient findOne(String id) {
+//        queryForObject()只返回一个对象,而不是List
+        return jdbc.queryForObject("select id, name, type from Ingredient where id = ?", this::mapRowToIngredient, id);
+    }
+
+    @Override
+    public Ingredient save(Ingredient ingredient) {
+//        update()执行向数据库写入或更新数据的查询语句
+        jdbc.update("insert into Ingredient (id, name, type) values(?, ?, ?)", ingredient.getId(), ingredient.getName(), ingredient.getType());
     }
 }
