f5ebde1fd104f1f5b57648e2cf1e333e4f1b7f1e
holdthebreath <hwdxxy@gmail.com>
2020-07-08 16:42:12 +0800

    章节2.3内容(添加Order类校验)

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,8 +1,11 @@
 package tacos;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 /**
  * @ClassName Order
@@ -15,11 +18,19 @@ import javax.validation.constraints.NotBlank;
 public class Order {
     @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Street is required")
     private String street;
+    @NotBlank(message = "City is required")
     private String city;
+    @NotBlank(message = "State is required")
     private String state;
+    @NotBlank(message = "Zip code is required")
     private String zip;
+    //合法的信用卡号,能通过Luhn算法检查
+    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 }
