f7d8043e111a97cdbd695da22a52dfc228138e86
holdthebreath <hwdxxy@gmail.com>
2020-07-14 17:32:16 +0800

    章节3.2内容(注解领域类为JPA实体)

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,7 +1,12 @@
 package tacos;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * @ClassName Ingredient
@@ -11,9 +16,19 @@ import lombok.RequiredArgsConstructor;
  * @Version 1.0
  */
 //lombok生成所有缺失的方法(getter,setter等),还会生成final属性作为参数等构造器
+//@Data隐式地添加了一个必需的有参构造函数,但是当使用@NoArgsConstructor 时,该构造函数将被删除。
 @Data
+//因此显式的@RequiredArgsConstructor确保除了私有无参数构造函数外,仍然有一个必需有参构造函数。
 @RequiredArgsConstructor
+//JPA要求实体需要一个无参构造函数,lombok注解实现这个
+//但不希望使用它,将access属性设为private
+//因为必须设置final属性,所以还要将force属性设置为true,这将导致Lombok生成的构造函数将它们设置为null
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+//将类声明为JPA实体
+@Entity
 public class Ingredient {
+    //id属性必须使用@Id注解,以便将其指定为唯一标识
+    @Id
     private final String id;
     private final String name;
     private final Type type;
--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -3,9 +3,17 @@ package tacos;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -18,9 +26,16 @@ import java.util.List;
  * @Version 1.0
  */
 @Data
-public class Order {
+@Entity
+//指定实体应该持久化的表名字(Taco_Order)
+//可以在任何实体类上使用这个注解
+//如果不使用,则JPA将默认将实体持久化到一个名为Order的表中
+@Table("Taco_Order")
+public class Order implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private Date PlaceAt;
+    private Date placeAt;
     @NotBlank(message = "Name is required")
     private String deliveryName;
     @NotBlank(message = "Street is required")
@@ -39,9 +54,15 @@ public class Order {
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
+    @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addDesign(Taco design) {
         this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placeAt() {
+        this.placeAt = new Date();
     }
 }
